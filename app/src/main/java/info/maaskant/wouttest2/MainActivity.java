3c59a61c92d711424e973147f9f317f1ca6e02c3
Wout <git@wout.maaskant.info>
2017-01-06 17:03:07 +0100

    feat: Disabled FAB

--- app/src/main/java/info/maaskant/wouttest2/MainActivity.java
+++ app/src/main/java/info/maaskant/wouttest2/MainActivity.java
@@ -1,23 +1,18 @@
 package info.maaskant.wouttest2;
 
+import javax.inject.Inject;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import javax.inject.Inject;
 
 import info.maaskant.wouttest2.data.DataFunctions;
 import info.maaskant.wouttest2.navigation.NavigationFragment;
@@ -46,10 +41,10 @@ public class MainActivity extends AppCompatActivity {
         createDrawer(toolbar);
         this.navigationFragment = getOrCreateNavigationFragment(savedInstanceState);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(
-                view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show());
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(
+//                view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show());
     }
 
     /**
