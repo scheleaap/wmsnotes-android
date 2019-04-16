c96434b50973fb51e690974f4cce623e3ba7e792
scheleaap <git@wout.maaskant.info>
2019-04-16 21:28:45 +0200

    chore: Automatically tag version after deployment

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+
+function get_version_number() {
+  cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
+}
+
+./gradlew clean bundleRelease
+if [ $TRAVIS_BRANCH = 'master' ]; then
+    bundle exec fastlane deploy
+
+    version_number=$(get_version_number)
+    if [ "$version_number" != "" ]; then
+      git tag -a "$version_number" -m"Version $version_number"
+    fi
+fi
