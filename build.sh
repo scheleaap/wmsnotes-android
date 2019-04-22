ce42a8d449456af1a67395514c29dc9ffc563cb3
scheleaap <git@wout.maaskant.info>
2019-04-22 18:26:44 +0200

    chore: Build script

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ function get_version_number() {
   cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
 }
 
-./gradlew clean bundleRelease
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+echo "Travis branch $TRAVIS_BRANCH"
+#./gradlew clean bundleRelease
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
     bundle exec fastlane deploy
 
     version_number=$(get_version_number)
     if [ "$version_number" != "" ]; then
-      git tag -a "$version_number" -m"Version $version_number"
+      echo "Tagging version $version_number"
+      # git tag -a "$version_number" -m"Version $version_number"
     else
       >&2 echo "Version number not tagged!"
     fi
