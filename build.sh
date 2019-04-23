748f83b7e941f82533e43e60e716840c14a6133a
scheleaap <git@wout.maaskant.info>
2019-04-23 08:19:45 +0200

    chore: Build script

--- build.sh
+++ build.sh
@@ -8,21 +8,20 @@ setup_git() {
 }
 
 function get_version_number_from_gradle() {
-  # cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
-  date +%s | sha256sum | base64 | head -c 32 ; echo
+  cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
 }
 
 echo "Building Travis branch $TRAVIS_BRANCH"
-#./gradlew clean bundleRelease
-if [[ "$TRAVIS_BRANCH" == "feature-buildscript" ]]; then
-    #bundle exec fastlane deploy
+./gradlew clean bundleRelease
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    bundle exec fastlane deploy
 
     version_number=$(get_version_number_from_gradle)
     if [[ "$version_number" != "" ]]; then
       echo "Tagging version $version_number"
       setup_git
-      git tag -a "test-$version_number" -m"Version $version_number"
-      git push https://${GITHUB_TOKEN}@github.com/scheleaap/wmsnotes-desktop-and-server.git "test-$version_number"
+      git tag -a "$version_number" -m"Version $version_number"
+      git push https://${GITHUB_TOKEN}@github.com/scheleaap/wmsnotes-android.git "$version_number"
     else
       >&2 echo "Version number not tagged!"
     fi
