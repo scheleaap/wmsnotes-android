1038d9dac36dafe5c359cc31ae4e1a7cc5e34c34
scheleaap <git@wout.maaskant.info>
2019-04-23 07:54:35 +0200

    chore: Build script

--- build.sh
+++ build.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+# The git stuff was based on this: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 set -ev
 
-function get_version_number() {
-  cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
+setup_git() {
+  git config --global user.name "Travis CI"
+}
+
+function get_version_number_from_gradle() {
+  # cat app/build.gradle | sed -rn 's/.*versionName "([0-9.]+)".*/\1/p'
+  date +%s | sha256sum | base64 | head -c 32 ; echo
 }
 
 echo "Building Travis branch $TRAVIS_BRANCH"
-./gradlew clean bundleRelease
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    bundle exec fastlane deploy
+#./gradlew clean bundleRelease
+if [[ "$TRAVIS_BRANCH" == "feature-buildscript" ]]; then
+    #bundle exec fastlane deploy
 
-    version_number=$(get_version_number)
-    if [ "$version_number" != "" ]; then
+    version_number=$(get_version_number_from_gradle)
+    if [[ "$version_number" != "" ]]; then
       echo "Tagging version $version_number"
-      git tag -a "$version_number" -m"Version $version_number"
-      git push origin "$version_number"
+      setup_git
+      git tag -a "test-$version_number" -m"Version $version_number"
+      git push https://${GITHUB_TOKEN}@github.com/scheleaap/wmsnotes-desktop-and-server.git "$version_number"
     else
       >&2 echo "Version number not tagged!"
     fi
