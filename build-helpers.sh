afda8de4dda4531198d64bb70e50ad9e692535c5
scheleaap <git@wout.maaskant.info>
2019-07-06 21:27:56 +0200

    chore: Automatically add changelogs if not present.

---
+++ build-helpers.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# functions to commits on a branch in a Travis CI build
+# be sure to avoid creating a Travis CI fork bomb
+# see https://github.com/travis-ci/travis-ci/issues/1701
+# based on https://gist.github.com/ddgenome/f3a60fe4c2af0cbe758556d982fbeea9
+
+function travis_checkout_branch() {
+    local head_ref branch_ref
+    head_ref=$(git rev-parse HEAD)
+    if [[ $? -ne 0 || ! $head_ref ]]; then
+        err "failed to get HEAD reference"
+        return 1
+    fi
+    branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
+    if [[ $? -ne 0 || ! $branch_ref ]]; then
+        err "failed to get $TRAVIS_BRANCH reference"
+        return 1
+    fi
+    if [[ $head_ref != $branch_ref ]]; then
+        msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
+        msg "someone may have pushed new commits before this build cloned the repo"
+        return 0
+    fi
+    if ! git checkout "$TRAVIS_BRANCH"; then
+        err "failed to checkout $TRAVIS_BRANCH"
+        return 1
+    fi
+}
+
+function travis_add_and_commit() {
+    if [[ $# -ne 2 ]]; then
+        err "not enough arguments"
+        return 1
+    fi
+    local add_path=$1
+    local commit_message=$2
+
+    git config --global user.name "Travis CI"
+    if ! git add --all $add_path; then
+        err "failed to add modified files to git index"
+        return 1
+    fi
+    # make Travis CI skip this build
+    if ! git diff --quiet $add_path && git diff --staged --quiet $add_path || git commit -m "$commit_message [ci skip]"; then
+        err "failed to commit updates"
+        return 1
+    fi
+}
+
+function travis_push() {
+    local remote=origin
+    if [[ $GITHUB_TOKEN ]]; then
+        remote=https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG
+    fi
+    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+        err "failed to push git changes"
+        return 1
+    fi
+}
+
+function msg() {
+    echo "travis-commit: $*"
+}
+
+function err() {
+    msg "$*" 1>&2
+}
--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 # The git stuff was based on this: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 set -ex
 
-setup_git() {
-  git config --global user.name "Travis CI"
-  git fetch --unshallow
-}
-
 function create_changelog_if_not_present() {
     if [[ "$#" -ne 2 ]]; then echo "Usage: $0 <version code> <language code>"; exit 1; fi
     local version_code="$1"
@@ -26,15 +21,8 @@ function create_changelogs_if_not_present() {
 
     create_changelog_if_not_present ${version_code} "en-US"
     create_changelog_if_not_present ${version_code} "nl-NL"
-    find
-    # The code below currently does not work on Travis, as it checks the repository out with a detached head.
-    #local changelog_path="fastlane/metadata/android/*/changelogs"
-    #git add ${changelog_path}
-    #git diff --quiet ${changelog_path} && git diff --staged --quiet ${changelog_path} \
-    #  || git commit -m "chore: Added default changelogs for version code ${version_code}." ${changelog_path} \
-    #    && git push https://${GITHUB_TOKEN}@github.com/scheleaap/wmsnotes-android.git \
-    #    && echo "Exiting build to let the next build take over." \
-    #    && return 1
+    local changelog_path="fastlane/metadata/android/*/changelogs"
+    travis_add_and_commit $changelog_path "chore: Added default changelogs for version code ${version_code}."
 }
 
 function get_version_code_from_gradle() {
@@ -50,20 +38,20 @@ function tag_git_with_version_number() {
 
     if [[ "$version_number" != "" ]]; then
       echo "Tagging version $version_number"
-      setup_git
-      git tag -a "$version_number" -m"Version $version_number"
-      git push https://${GITHUB_TOKEN}@github.com/scheleaap/wmsnotes-android.git "$version_number"
+      git tag -a "$version_number" -m"Version $version_number [ci skip]"
+      travis_push
     else
       >&2 echo "Version number not tagged!"
     fi
 }
 
 echo "Building Travis branch ${TRAVIS_BRANCH}"
+travis_checkout_branch
 version_code=$(get_version_code_from_gradle)
 version_number=$(get_version_number_from_gradle)
 create_changelogs_if_not_present ${version_code}
-./gradlew clean bundleRelease
-if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-    bundle exec fastlane deploy
-    tag_git_with_version_number ${version_number}
-fi
+#./gradlew clean bundleRelease
+#if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+#    bundle exec fastlane deploy
+#    tag_git_with_version_number ${version_number}
+#fi
