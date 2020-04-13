4fd5fa9fc147e7c4412947d02c91b81f76ea1df6
scheleaap <git@wout.maaskant.info>
2020-04-13 17:41:04 +0200

    chore: Attempt to get release working again after Google Play API upgrade

--- build.sh
+++ build.sh
@@ -54,8 +54,8 @@ source $basedir/build-helpers.sh
 version_code=$(get_version_code_from_gradle)
 version_number=$(get_version_number_from_gradle)
 create_changelogs_if_not_present ${version_code}
-#./gradlew clean bundleRelease
+./gradlew clean bundleRelease
 if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-#    bundle exec fastlane deploy version_code:${version_code}
+    bundle exec fastlane deploy version_code:${version_code}
     tag_git_with_version_number ${version_number}
 fi
